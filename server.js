df53058ae218750319cfd93da8bdc41876ba8395
DasH <fxfddfno@gmail.com>
2020-02-03 17:06:43 +0200

    setup our routes

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const express = require('express');
 const app = express();
 
 
+const users = require("./routes/users.js");
+const auth = require('./routes/auth');
+const contacts = require("./routes/contacts");
+
+
+app.use('/api/v1/auth',users)
+app.use('/api/v1/users', auth)
+app.use('/api/v1/contacts', contacts)
+
+
 const port = process.env.PORT || 5000;
 app.listen(port, ()=>{
     console.log(`Server started on port ${port}`)
