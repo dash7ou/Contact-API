2096ea43f6fe8dfeab468f35c8a15cf33afa2034
DasH <fxfddfno@gmail.com>
2020-02-05 22:46:27 +0200

    add to navbar

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import About from "./components/pages/About";
 import ContactState from "./context/contact/ContextState";
 import AuthState from "./context/auth/AuthState";
 import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 
 import './App.css';
@@ -22,6 +23,7 @@ function App() {
                 <Route exact path="/" component={Home}/>
                 <Route exact path="/about" component={About} />
                 <Route exact path="/register" component={Register}/>
+                <Route exact path="/login" component={Login} />
               </Switch>
             </div>
           </Fragment>
