7751308f419ea7835a088480066acdbef3e57d77
DasH <fxfddfno@gmail.com>
2020-02-05 20:38:17 +0200

    add transation

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,4 +1,5 @@
 import React, {Fragment, useContext} from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group"
 import ContactContext from "../../context/contact/contactContext"
 import ContactItem from "./ContactItem";
 
@@ -10,24 +11,28 @@ const Contacts = ()=>{
     }
     return(
         <Fragment>
-            {
-                contactFiltered ? contactFiltered.map(contact => 
-                    <ContactItem  
-                        contact={contact}
-                        deleteContact={deleteContact}
-                        setCurrent={setCurrent}
-                        key={contact.id}
-                    />
-                ):
-                contacts.map(contact => 
-                    <ContactItem  
-                        contact={contact}
-                        deleteContact={deleteContact}
-                        setCurrent={setCurrent}
-                        key={contact.id}
-                    />
-                )
-            }
+            <TransitionGroup>
+                {
+                    contactFiltered ? contactFiltered.map(contact =>
+                        <CSSTransition key={contact.id} timeout={500} classNames="item">
+                            <ContactItem  
+                                contact={contact}
+                                deleteContact={deleteContact}
+                                setCurrent={setCurrent}
+                            />
+                        </CSSTransition>
+                    ):
+                    contacts.map(contact =>
+                        <CSSTransition key={contact.id} timeout={500} classNames="item">
+                            <ContactItem  
+                                contact={contact}
+                                deleteContact={deleteContact}
+                                setCurrent={setCurrent}
+                            />
+                        </CSSTransition>
+                    )
+                }
+            </TransitionGroup>
         </Fragment>
     )
 }
