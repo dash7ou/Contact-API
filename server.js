f6b6e61b2f67ac6a19a77e906285b7f58c2dfdd5
DasH <fxfddfno@gmail.com>
2020-02-07 17:29:23 +0200

    prepare to deploy

--- server.js
+++ server.js
@@ -15,6 +15,14 @@ app.use('/api/v1/auth',auth)
 app.use('/api/v1/users', users)
 app.use('/api/v1/contacts', contacts)
 
+// server static assets in production
+if(process.env.NODE_ENV === 'production'){
+    // Set static folder...
+    app.use(express.static('client/build'))
+
+    app.use('*', (req, res)=> res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
+
 // handle errors
 app.use(errorHandler);
 
